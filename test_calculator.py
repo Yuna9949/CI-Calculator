6e7476bc61470a9668657ae2fdbfc18951fda7f2
Yuna Oh <ustina0409@gmail.com>
2021-06-06 12:54:54 -0700

    division method and test

--- test_calculator.py
+++ test_calculator.py
@@ -13,3 +13,7 @@ class TestCalculatorApp:
 
     def test_multiply(self):
         assert 4 == calculator.multiply(2, 2)
+
+    def test_division(self):
+        assert 4 == calculator.division(8, 2)
+        
